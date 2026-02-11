cc7942bd763caad84d18641dbc3e4f1849b95650
JalenG22 <157394322+JalenG22@users.noreply.github.com>
2026-02-11 18:22:55 -0500

    Added Backup script

---
+++ backup.sh
@@ -0,0 +1,18 @@
+#Backup Script#
+
+#!/bin/bash
+
+#Folder to backup 
+SOURCE_FOLDER="my_folder"
+
+#Backup Folder Destination#
+BACKUP_FOLDER="backup"
+
+#Create backup folder if one does not exist#
+mkdir -p $BACKUP_FOLDER
+
+#Creates a backup that is timestamped#
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
+tar -czf "$BACKUP_FOLDER/backup_$TIMESTAMP.tar.gz" $SOURCE_FOLDER
+
+echo "Backup of $SOURCE_FOLDER completed at $TIMESTAMP"
