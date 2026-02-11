b3f991329c7d318901f2a9b9d60f809134137b3a
JalenG22 <157394322+JalenG22@users.noreply.github.com>
2026-02-11 18:45:54 -0500

    Added Disk usage monitoring script

---
+++ disk_usage.sh
@@ -0,0 +1,16 @@
+#Disk Usage
+
+#!/bin/bash
+
+#Threshold in percentage
+THRESHOLD=80
+
+#Get disk usage of root partition
+USAGE=$(df / | tail -1 | awk '{print $5}'
+
+if [ $USAGE -ge $THRESHOLD ]; then 
+  echo "Warning: Disk usage is at ${USAGE}%"
+else
+ echo "Disk usage is safe: ${USAGE}%"
+ fi
+ 
