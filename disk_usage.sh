e6c60ae9f49f9492c6ce8d15e7dbdc2562042ffc
JalenG22 <157394322+JalenG22@users.noreply.github.com>
2026-02-11 19:11:45 -0500

    Update disk_usage.sh

--- disk_usage.sh
+++ disk_usage.sh
@@ -6,7 +6,7 @@
 THRESHOLD=80
 
 #Get disk usage of root partition
-USAGE=$(df / | tail -1 | awk '{print $5}'
+USAGE=$(df / | tail -1 | awk '{print $5}' | sed 's/%//')
 
 if [ $USAGE -ge $THRESHOLD ]; then 
   echo "Warning: Disk usage is at ${USAGE}%"
